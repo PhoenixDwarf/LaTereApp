e72fb3bff75ea8cc1282142f180cb1bab4ec7e5e
Edwin <95711147+PhoenixDwarf@users.noreply.github.com>
2022-04-16 18:59:22 -0500

    Added Register Page
    Added Register page
    Added new "Hamburguesa" png img
    Added new styles

--- src/app/tab1/pages/register/register.module.ts
+++ src/app/tab1/pages/register/register.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { RegisterPageRoutingModule } from './register-routing.module';
 
 import { RegisterPage } from './register.page';
+import { ShowHidePasswordComponent } from '../../components/show-hide-password/show-hide-password.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { RegisterPage } from './register.page';
     IonicModule,
     RegisterPageRoutingModule
   ],
-  declarations: [RegisterPage]
+  declarations: [RegisterPage,ShowHidePasswordComponent]
 })
 export class RegisterPageModule {}
